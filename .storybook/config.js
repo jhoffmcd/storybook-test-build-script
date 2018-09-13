b25243b255089eac5d291d3a5f6f5dfd3fba9ba0
Jesse Hoffman <jhoffmcd@gmail.com>
2018-09-12 17:24:06 -0700

    Update alternative env var test scripts

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,11 @@ if (process.env.NODE_ENV === "test") {
   require("babel-plugin-require-context-hook/register")();
 }
 
+// Alternate test with STORYBOOK environment variable
+// if (process.env.STORYBOOK_BUILD !== "true") {
+//   require("babel-plugin-require-context-hook/register")();
+// }
+
 // Dynamically load all files within src folder `{componentName}.stories.js` pattern
 const requireComponents = require.context("../src", true, /stories\.js$/);
 
